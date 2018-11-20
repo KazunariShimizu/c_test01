8e68c12a0fcbd53af2d61203c2d9a860b6611015
KazunariShimizu <shimizu@tora-v.net>
2018-11-20 11:56:08 +0900

    文字列とポインタのプログラム例

---
+++ array_char.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char name1[7] = "Ichiro";
+    char *name2 = "Jiro";
+    printf("私の名前は%sです。\n",name1);
+    printf("私の名前は%sです。\n",name2);
+    return 0;
+}
