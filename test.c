3f69e64eb6679501f37c8d5c3b6b3bb4d948591c
KazunariShimizu <shimizu@tora-v.net>
2018-11-20 10:37:01 +0900

    インクリメント演算子　デクリメント演算子

--- test.c
+++ test.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 
-void main(void){
-	printf("Hello World");
-
+int main(void){
+	int a = 0,b = 0,i = 1,j = 1;
+	a = i++;
+	b = ++j;
+	
+	printf("a=%d,b=%d",a,b);
+	return 0;
 }
 
